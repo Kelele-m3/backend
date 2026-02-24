1414bbfb203ab67868b9f53c371aeae8789bb28f
Kelele <kelele@elifetransfer.com>
2026-02-24 19:36:33 +0300

    Feature 4: Article update

--- src/articles/dto/update-article.dto.ts
+++ src/articles/dto/update-article.dto.ts
@@ -1,32 +1,30 @@
-import {
-  IsString,
-  IsEnum,
-  IsOptional,
-  MinLength,
-  MaxLength,
-  IsIn,
-} from 'class-validator';
+import { IsString, IsEnum, IsOptional, MinLength, MaxLength, IsIn, } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { ArticleStatus } from '../entities/article.entity';
 
 const CATEGORIES = ['Politics', 'Tech', 'Sports', 'Health'] as const;
 
 export class UpdateArticleDto {
+  @ApiPropertyOptional({ description: 'Article title', maxLength: 150 })
   @IsOptional()
   @IsString()
   @MinLength(1)
   @MaxLength(150)
   title?: string;
 
+  @ApiPropertyOptional({ description: 'Article content', minLength: 50 })
   @IsOptional()
   @IsString()
   @MinLength(50)
   content?: string;
 
+  @ApiPropertyOptional({ description: 'Article category', enum: CATEGORIES })
   @IsOptional()
   @IsString()
   @IsIn([...CATEGORIES])
   category?: string;
 
+  @ApiPropertyOptional({ description: 'Article status', enum: ArticleStatus })
   @IsOptional()
   @IsEnum(ArticleStatus)
   status?: ArticleStatus;
